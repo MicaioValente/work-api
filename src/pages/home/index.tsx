c14d8b359ef7a638683c9db4603dcff14b8fb8b9
Micaio Tomichi <micaio.tomichi@workdb.com.br>
2022-10-13 11:15:40 -0300

    is not nill

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -92,7 +92,9 @@ export default function Home() {
   }, [timerTool]);
 
   const InitialDate = (e: moment.Moment | null) => {
-    setDataInicio(e);
+    if (e) {
+      setDataInicio(e);
+    }
   };
   useEffect(() => {
     switch (tipoContrato) {
